4c93106db707acbaf38d76c974bda99c927a057f
Binaaa04 <sabrinarahmadini60gmail.com>
2023-10-22 05:45:01 +0700

    Selection 1

---
+++ Selection1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Selection1
+ */
+public class Selection1 {
+
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        System.out.println("Input a number = ");
+        int number = input21.nextInt();
+        if (number%2 == 0) {
+            System.out.println(number+" is an even number! ");
+        } else {
+            System.out.println(number+" is an odd number! ");
+        }
+    }
+}
