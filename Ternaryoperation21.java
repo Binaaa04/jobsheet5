187b3af1e9c88e7488ff996aa79ba74d93890455
Binaaa04 <sabrinarahmadini60gmail.com>
2023-10-22 15:03:45 +0700

    Ternary Operator

---
+++ Ternaryoperation21.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/**
+ * Ternaryoperation21
+ */
+public class Ternaryoperation21 {
+
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        System.out.println("Input a number = ");
+        int number = input21.nextInt();
+        int even = 2;
+        if (number%2 == 0) {
+            String results = number >= even? "even number":"odd number";
+            System.out.println(number+" is an even number! ");
+        } else {
+            System.out.println(number+" is an odd number! ");
+        }
+    }
+}
