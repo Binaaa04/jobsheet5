4b09200c97efb608fe42051290a046e19aa34e78
Binaaa04 <sabrinarahmadini60gmail.com>
2023-10-22 14:50:59 +0700

    Ternary Operator

--- Selection1.java
+++ Selection1.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
  */
 public class Selection1 {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         System.out.println("Input a number = ");
         int number = input21.nextInt();
+        int even = 2;
         if (number%2 == 0) {
+            String results = number >= even? "even number":"odd number";
             System.out.println(number+" is an even number! ");
         } else {
             System.out.println(number+" is an odd number! ");
         }
+  
+        
     }
 }
