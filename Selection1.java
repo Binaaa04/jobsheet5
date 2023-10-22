7a945f1304a506cc43ac92f07d6e7ee505d2eed1
Binaaa04 <sabrinarahmadini60gmail.com>
2023-10-22 15:05:59 +0700

    Selection pertama

--- Selection1.java
+++ Selection1.java
@@ -12,9 +12,7 @@ public class Selection1 {
         Scanner input21 = new Scanner(System.in);
         System.out.println("Input a number = ");
         int number = input21.nextInt();
-        int even = 2;
         if (number%2 == 0) {
-            String results = number >= even? "even number":"odd number";
             System.out.println(number+" is an even number! ");
         } else {
             System.out.println(number+" is an odd number! ");
