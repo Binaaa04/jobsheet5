9eb069772ce91d92546956424b293688801f5178
Binaa <143788742+Binaaa04@users.noreply.github.com>
2023-11-05 23:27:03 +0700

    Modify case study exercise 2

---
+++ Modifycasestudy21.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+/**
+ * Modifycasestudy21
+ */
+public class Modifycasestudy21 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner (System.in);
+        System.out.println("Masukkan Angka : ");
+        int angka=input21.nextInt();
+
+        if (80<angka&&angka<=100) {
+            System.out.println("Anda mendapatkan A dengan nilai 4 dan kualifikasi SANGAT BAIK");
+        } else if (73<angka&&angka<=80) {
+            System.out.println("Anda mendapatkan B dengan nilai 3.5 dan kualifikasi LEBIH DARI BAIK");
+        } else if (65<angka&&angka<=73) {
+            System.out.println("Anda mendapatkan  dengan nilai 3 dan kualifikasi BAIK");
+        } else if (60<angka&&angka<=65) {
+             System.out.println("Anda mendapatkan C+ dengan nilai 2.5 dan kualifikasi LEBIH DARI CUKUP");    
+        } else if (50<angka&&angka<=60) {
+            System.out.println("Anda mendapatkan C dengan nilai 2 dan kualifikasi CUKUP");
+        } else if (39<angka&&angka<=50) {
+            System.out.println("Anda mendapatkan D dengan nilai 1 dan kualifikasi KURANG");
+        } else if (angka<=39) {
+            System.out.println("Anda mendapatkan E dengan nilai 0 dan kualifikasi GAGAL");
+        } 
+        }{
+            
+        } {
+            
+        } {
+            
+        } {
+            
+        } {
+            
+        } {
+            
+        }
+        }
+       
+    
